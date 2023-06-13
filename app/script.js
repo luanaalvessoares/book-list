4216a6e388d089d8022d7859a46c63f1e74a071e
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-13 09:48:24 -0300

    feat: addBookToList method

--- app/script.js
+++ app/script.js
@@ -5,3 +5,16 @@ class Book {
         this.isbn = isbn;
     }
 }
+
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${book.title}</td>
+                         <td>${book.author}</td>
+                         <td>${book.isbn}</td>
+                         <td><a href="" class="delete">X</a></td>`;
+    
+        list.appendChild(row);
+    }
+}
